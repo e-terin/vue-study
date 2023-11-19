648b7910786bee0167f5efeebda516bb03de8ee7
Evgeniy Terin <web@derin.ru>
2023-11-19 13:23:29 +0300

    Likes/Dislikes

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button @click="addLike">Add Like</button>
+  <button @click="addDislike">Add Dislike</button>
+  <div>
+    {{ likes }}<br>
+    {{ dislikes }}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0
+    }
+  },
+  methods: {
+    addLike(){
+      this.likes++
+    },
+    addDislike(){
+      this.dislikes++
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
